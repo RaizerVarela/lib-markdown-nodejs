4b9bcd51865f0113870c4a918c6e62ce6107be08
Raizer Varela <raizer.varela@gmail.com>
2022-05-24 10:03:44 -0300

    Alteração - Pegando arquivo .md com fs e exibindo no console

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
+const fs = require('fs');
 const chalk = require("chalk");
 
-console.log(chalk.blue("Pronto para começar!"));
-
-const paragrafo = "Texto executado por função";
-
-function texto(string) {
-  return string;
+function getError(err){
+  throw new Error(chalk.red(err.code, 'ARQUIVO NÃO ENCONTRADO'))
 }
 
-console.log(texto(paragrafo));
+function getFile(path){
+  const encoding = 'utf-8'
+  fs.readFile(path, encoding, (err, data) => {
+    if (err){
+      getError(err)
+    } else{
+      console.log(chalk.green(data))
+    }
+  })
+}
+
+getFile('./arquivos/texto1.md')
